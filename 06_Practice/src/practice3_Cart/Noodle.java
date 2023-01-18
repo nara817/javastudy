403027a2bb6c800cb1a1c5466aba6681d008de18
nara817 <knr817@naver.com>
2023-01-18 19:51:16 +0900

    2023-01-18

---
+++ 06_Practice/src/practice3_Cart/Noodle.java
@@ -0,0 +1,13 @@
+package practice3_Cart;
+
+public class Noodle extends Product{
+
+	// new Noodle(3, "누들면", 1500);
+	// 생성자
+	public Noodle(int prodNo, String prodName, int prodPrice) {
+		super(prodNo, prodName, prodPrice);
+	
+	}
+
+}
+
