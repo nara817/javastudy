ad2b7afb1e58154ad92c72323c4086c789e60183
nara817 <knr817@naver.com>
2023-01-26 16:46:08 +0900

    2023-01-26

--- 09_IO/src/ex02_Writer/JSONMainClass.java
+++ 09_IO/src/ex02_Writer/JSONMainClass.java
@@ -73,8 +73,60 @@ public class JSONMainClass {
 		
 	}
 
+	public static void ex03() {
+		
+		// String 형식의 JSON 데이터
+		String str = "{\"name\":\"james\",\"age\":30,\"height\":180.5,\"isAlive\":true}";
+		
+		// JSONObject 객체 생성
+		JSONObject obj = new JSONObject(str);
+		
+		// JSONObject 객체를 구성하는 프로퍼티 값 가져오기
+		String name = obj.getString("name");
+		int age = obj.getInt("age");
+		double height = obj.getDouble("height");
+		boolean isAlive = obj.getBoolean("isAlive");
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(height);
+		System.out.println(isAlive);
+		
+	}
+	
+	public static void ex04() {
+		
+		// [{"model":"세탁기","maker":"삼성","price":100}] String 밖에서 선 입력 후
+		// String str = ""; →  이 안에 붙여 넣으면 자동으로 큰따옴표 입혀짐)
+		// String 형식의 JSON 데이터
+		String str = "[{\"model\":\"세탁기\",\"maker\":\"삼성\",\"price\":100},{\"model\":\"냉장고\",\"maker\":\"LG\",\"price\":200}]";
+		
+		// JSONArray 객체 생성
+		JSONArray products = new JSONArray(str);
+		
+		// 1. 일반 for문
+		for(int i = 0; i < products.length(); i++) {
+			JSONObject obj = products.getJSONObject(i);
+			String model = obj.getString("model");
+			String maker = obj.getString("maker");
+			int price = obj.getInt("price");
+			System.out.println(model + "," + maker + "," + price);
+		}
+		
+		// 2. 향상 for문
+		// 변수:배열
+		for(Object obj : products) {
+			JSONObject product = (JSONObject)obj; // obj → JSONObject 캐스팅
+			String model = product.getString("model");
+			String maker = product.getString("maker");
+			int price = product.getInt("price");
+			System.out.println(model + "," + maker + "," + price);
+			
+		}
+		
+	}
 	public static void main(String[] args) {
-		ex02();
+		ex04();
 
 	}
 
